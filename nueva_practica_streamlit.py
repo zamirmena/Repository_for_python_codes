e28c5aaf28c2532e92327be53daca7e1b8a6889e
zamirmena <94096106+zamirmena@users.noreply.github.com>
2025-03-12 00:27:22 +0000

    V1

---
+++ nueva_practica_streamlit.py
@@ -0,0 +1,8 @@
+#Nueva practica
+#a = 7 #
+#c = 7
+#b = a + c
+#print (b)
+
+import streamlit as st 
+st.write("Hello World")
---
+++ preuba_streamlit.py
@@ -0,0 +1,10 @@
+a = 2
+b = 3
+c = a + b 
+print (c)
+print (c)
+
+import streamlit as st
+
+st.write('Hello World')
+
